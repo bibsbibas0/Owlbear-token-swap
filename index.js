e1554027f15151faa542f90b0e2195704ef7e5b5
bibsbibas0 <danielmmedeiros24@gmail.com>
2025-08-03 13:45:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-import OBR from "@owlbear-rodeo/sdk";
+// NÃO usa import!
+// import OBR from "@owlbear-rodeo/sdk";
+
+console.log("Extensão Token Swap ativada!");
 
 OBR.onReady(() => {
-  OBR.tokenControls.create({
-    id: "token-swap-button",
-    filter: {
-      every: [{ key: "layer", value: "CHARACTER" }]
-    },
-    controls: [
+  OBR.contextMenu.create({
+    id: "token-swap",
+    icons: [
       {
-        id: "swap-button",
         icon: "https://cdn-icons-png.flaticon.com/512/1828/1828911.png",
-        tooltip: "Trocar Token",
-        onClick: (context) => {
-          for (const token of context.items) {
-            const estados = token.metadata["daniel.estados"] || [];
-            const atual = estados.indexOf(token.image.url);
-            const proximo = estados[(atual + 1) % estados.length];
+        label: "Trocar token",
+        filter: {
+          every: []
+        }
+      }
+    ],
+    onClick(context) {
+      for (const token of context.items) {
+        const estados = token.metadata["daniel.estados"] || [];
+        const atual = estados.indexOf(token.image.url);
+        const proximo = estados[(atual + 1) % estados.length];
 
-            OBR.scene.items.updateItems([{
-              id: token.id,
-              image: { url: proximo },
-              metadata: {
-                "daniel.estados": estados
-              }
-            }]);
+        OBR.scene.items.updateItems([{
+          id: token.id,
+          image: { url: proximo },
+          metadata: {
+            "daniel.estados": estados
+          }
+        }]);
+      }
+    }
+  });
+});
+
           }
         }
       }
